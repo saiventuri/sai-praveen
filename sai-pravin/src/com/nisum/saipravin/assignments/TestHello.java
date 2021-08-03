a6570dd47636539c36184ddab8c07bd2948e328e
saiventuri <sventuri@nisum.com>
2021-08-03 19:18:52 +0530

    Test commit

--- sai-pravin/src/com/nisum/saipravin/assignments/TestHello.java
+++ sai-pravin/src/com/nisum/saipravin/assignments/TestHello.java
@@ -3,6 +3,6 @@ package com.nisum.saipravin.assignments;
 public class TestHello {
 
 	public static void main(String[] args) {
-		System.out.println("Hi...");
+		System.out.println("Hello World...");
 	}
 }
